f8299f5988058d73f1ea5e6f8856756940cf6b99
Trevor Brooks <trevorism@gmail.com>
2023-09-02 18:06:37 -0400

    Trying to figure out why plugin is not initializing properly

--- src/main/groovy/com/trevorism/plugin/tasks/InitializeVersioningTask.groovy
+++ src/main/groovy/com/trevorism/plugin/tasks/InitializeVersioningTask.groovy
@@ -2,32 +2,40 @@ package com.trevorism.plugin.tasks
 
 import com.trevorism.plugin.ext.VersioningSettings
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
 class InitializeVersioningTask extends DefaultTask{
 
-    @OutputFile
+    @InputFile
     def gradlePropertiesFile = project.file("gradle.properties")
 
     @TaskAction
     void initVersionSystem(){
         def version = getInitialVersion()
+        project.logger.info("Initializing version to ${version}")
 
         if(gradlePropertiesFile.exists()){
+            project.logger.info("Gradle properties file already exists. Updating it.")
             def text = gradlePropertiesFile.text
             if(!text.contains("version=")){
+                project.logger.info("Adding version to gradle properties file")
                 text << "\nversion=${version}"
             }
             if(!text.contains(VersioningSettings.NEXT_VERSION_KEY)){
+                project.logger.info("Adding next version strategy to gradle properties file")
                 text << "\n${VersioningSettings.NEXT_VERSION_KEY}=${VersioningSettings.PATCH}"
             }
+            project.logger.info("Setting properties file text to ${text}")
+
             gradlePropertiesFile.text = text
         }
         else{
             gradlePropertiesFile.createNewFile()
             String content = "version=${version}\n${VersioningSettings.NEXT_VERSION_KEY}=${VersioningSettings.PATCH}"
+            project.logger.info("Creating a new gradle.properties file with content ${content}")
             gradlePropertiesFile << content
         }
         project.version = version
